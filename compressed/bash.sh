60d0df47cdb70ca75143072dc65831e9348073f2
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-05-13 14:06:21 +0700

    update ACESFilmicToneMapping and basemesh

--- compressed/bash.sh
+++ compressed/bash.sh
@@ -1,20 +1,22 @@
 npm install -g @gltf-transform/cli
 
-gltf-transform uncompress facecap.glb facecap.gltf
-
 gltf-transform inspect facecap.gltf
 
 
 gltf-pipeline -i facecap.glb -o facecap.gltf --json
 
 
-gltf-transform cp facecap.gltf facecap_output.gltf
-
 
 ./basisu -ktx2 -decode my_texture.ktx2
 
 ktx-uninstall
 
-toktx
 
 ktx extract image baseColor.ktx2 baseColorOutput.png
+
+
+
+gltf-transform copy facetoy.glb facetoy.gltf --format
+gltf-pipeline -i facetoy.glb -o facetoy.gltf
+
+gltf-pipeline -i facetoy.gltf -t
