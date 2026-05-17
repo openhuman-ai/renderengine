1f3eb9631328f21a986fa18f4054b91f860d3de1
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2026-05-18 03:05:53 +0700

    update sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,31 +4,19 @@ import path from 'path';
 const rootDir = process.cwd();
 const outputPath = path.join(rootDir, 'public', 'sitemap.xml');
 const siteUrl = (process.env.SITE_URL || process.env.URL || 'https://openhuman.ai').replace(/\/+$/, '');
-const developerSiteUrl = process.env.DEVELOPER_SITE_URL || 'https://developer.openhuman.ai';
-const sdkSiteUrl = process.env.SDK_SITE_URL || 'https://sdk.openhuman.ai';
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<sitemap>
-		<loc>${developerSiteUrl}/sitemap-0.xml</loc>
-	</sitemap>
-
-	<sitemap>
-		<loc>${sdkSiteUrl}/sitemap.xml</loc>
-	</sitemap>
-</sitemapindex>
-
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
 		<loc>${siteUrl}/</loc>
 	</url>
 
 	<url>
-		<loc>${sdkSiteUrl}/</loc>
+		<loc>https://sdk.openhuman.ai/</loc>
 	</url>
 
 	<url>
-		<loc>${developerSiteUrl}/</loc>
+		<loc>https://developer.openhuman.ai/</loc>
 	</url>
 </urlset>
 `;
